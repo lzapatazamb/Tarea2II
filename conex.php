e4eeec199f10f3f24278e21cd5f0cab0b4838658
Laura <lzapatazamb@uniminuto.edu.co>
2019-02-12 08:04:51 -0500

    C8

--- conex.php
+++ conex.php
@@ -3,6 +3,10 @@ $conexion = mysql_connect("localhost", "lzapatazamb","");
 
 mysql_database("formulario",$conexion);
 
-// $query = mysql_query("select * from tabla",$conexion); 
+$Nombre=$POST['Nombre'];
+$Email=$POST['Email'];
+
+mysql_query("INSERT TO formulario (Nombre,Email) VALUES('$Nombre','$Email')");
+// $query = mysql_query("select * from tabla",$conexion);
 $resultado = ($query,0);
  ?>
