5c9653b73f7851a4bf69066fe6c2b7c929d6b21c
Laura <lzapatazamb@uniminuto.edu.co>
2019-02-12 08:01:13 -0500

    C7

--- conex.php
+++ conex.php
@@ -3,7 +3,6 @@ $conexion = mysql_connect("localhost", "lzapatazamb","");
 
 mysql_database("formulario",$conexion);
 
-$query = mysql_query("select * from tabla",$conexion);
-
+// $query = mysql_query("select * from tabla",$conexion); 
 $resultado = ($query,0);
  ?>
