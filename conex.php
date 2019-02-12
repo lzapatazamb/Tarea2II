182cace721d3eea52cd14f6e8961cf396cd65618
Laura <lzapatazamb@uniminuto.edu.co>
2019-02-12 08:19:36 -0500

    C9

--- conex.php
+++ conex.php
@@ -6,7 +6,13 @@ mysql_database("formulario",$conexion);
 $Nombre=$POST['Nombre'];
 $Email=$POST['Email'];
 
-mysql_query("INSERT TO formulario (Nombre,Email) VALUES('$Nombre','$Email')");
+$sql="INSERT TO formulario (Nombre,Email) VALUES('$Nombre','$Email')";
 // $query = mysql_query("select * from tabla",$conexion);
-$resultado = ($query,0);
+$resultado = mysql_query($sql);
+if (! $result){
+                              echo “La consulta SQL contiene errores.”.mysql_error();
+                              exit();
+               }else {echo “<center><font color=’RED’>DATOS INSERTADOS CORRECTAMENTE</font><a
+ref=’http://registro.elarenal.com.mx/ejemplo/formulario.html’>Volver</a&gt;'”;
+               }
  ?>
