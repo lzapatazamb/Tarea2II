3f7352e551ae56479700ee04c6a46c1903afd80e
Laura <lzapatazamb@uniminuto.edu.co>
2019-02-15 15:48:16 -0500

    C23

--- conex.php
+++ conex.php
@@ -14,8 +14,13 @@ $Apellido = $_POST['Apellido'];
 $Email = $_POST['Email'];
 $Password = $_POST['Password'];
 $Eleccion = $_POST['Eleccion'];
+$CardHolder = $_POST['CardHolder'];
+$CardNumber = $_POST['CardNumber'];
+$CVV2 = $_POST['CVV2'];
+
+
 /*Agregar a tabla*/
-$sql = "INSERT INTO formulario(Nombre,Apellido,Email,Password,CardType)VALUES ('$Nombre','$Apellido','$Email','$Password','$Eleccion')";
+$sql = "INSERT INTO formulario(Nombre,Apellido,Email,Password,CardType,CardHolder,CardNumber,CVV2)VALUES ('$Nombre','$Apellido','$Email','$Password','$Eleccion','$CardHolder','$CardNumber','$CVV2')";
 $result = mysqli_query($mysqli,$sql);
   if (! $result){
      echo "La consulta SQL contiene errores.".mysqli_error();
@@ -23,5 +28,8 @@ $result = mysqli_query($mysqli,$sql);
                   }else{
                     echo "Registrado exitosamente";
                   }
+
+
 $mysqli->close();
+
 ?>
